13c84560a73e76e463afb2bb9b227db47e76b336
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-18 03:37:13 +0300

    add

---
+++ 000-dart-syntax/001-variables/bin/main.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name = "fua";
+  int age = 30;
+  bool isActive = true;
+  double balance = 2232.00;
+
+  print("name: ${name}, age: ${age}, active: ${isActive}, balance: ${balance}");
+}
