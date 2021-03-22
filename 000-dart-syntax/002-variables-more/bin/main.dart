c6cc313be38d288eefeb2f7e7d062eef35634aab
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-03-23 01:54:43 +0300

    add

---
+++ 000-dart-syntax/002-variables-more/bin/main.dart
@@ -0,0 +1,17 @@
+var name = 'fua';
+var age = 30;
+var balance = 365547878.00;
+var is_single = false; //maybe ))
+var fav = [
+  'PS5',
+  'iphone.12',
+  'galaxy-z',
+  'mercedes-benz.cla',
+  'ice-cream',
+  'rog-strix-III'
+];
+
+void main() {
+  print(
+      "name: ${name}, age: ${age}, balance: ${balance}, is_single: ${is_single},\n favourite ${fav}");
+}
